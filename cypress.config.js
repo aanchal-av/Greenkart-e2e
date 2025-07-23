d6283c0f18b14b9d666291383b8ce39a0f2e57e1
Aanchal <aanchalverma.av@outlook.com>
2025-07-23 14:54:15 +0530

    Updated the config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,24 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://rahulshettyacademy.com/seleniumPractise/#/",
+    specPattern: "cypress/e2e/**/*.feature",
+    supportFile: false,
+    stepDefinitions: "cypress/e2e/**/*.steps.{js,ts}",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Cucumber preprocessor plugin
+      addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
     },
   },
 });
